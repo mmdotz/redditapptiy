741c4cae9dab62499055b81a5bfc309d05beeea6
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-03 13:56:45 -0500

    custom login routes, authenticate methods in app controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
 
-  root 'posts#index'
+  get '/login',       to: 'sessions#new',     as: 'new_login'
+  post '/login',      to: 'sessions#create',  as: 'create_session'
+  delete '/logout',   to: 'sessions#destroy', as: 'logout'
 
+  root 'posts#index'
 
 end
