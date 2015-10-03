cf3c592a41b0e0cdd4474b5b05b1fae4d4179cc3
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-03 15:27:13 -0500

    migrate correction to password default value

--- db/migrate/20151003202238_correct_default_to_users.rb
+++ db/migrate/20151003202238_correct_default_to_users.rb
@@ -1,6 +1,5 @@
-class AddDefaultToUsers < ActiveRecord::Migration
+class CorrectDefaultToUsers < ActiveRecord::Migration
   def change
     change_column_default :users, :password_digest, :null => false
   end
 end
-
