5343fc04868e46f4489386b2bec7c2e799576086
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-12 16:04:12 -0500

    correct nil user posts for ajax post

--- db/migrate/20151003150358_add_default_to_users.rb
+++ db/migrate/20151003150358_add_default_to_users.rb
@@ -3,3 +3,5 @@ class AddDefaultToUsers < ActiveRecord::Migration
     change_column_default :users, :password_digest, :null => false
   end
 end
+
+#change_column_null :users, :password_digest, false
